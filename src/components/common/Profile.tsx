2cdde3c31a1a679da39769f74a591575dbc1dfdf
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-03-04 17:21:49 +0530

    receive data from agent profilePage

--- src/components/common/Profile.tsx
+++ src/components/common/Profile.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Email, Phone, Place } from "@mui/icons-material";
+import { Box, Stack, Typography } from "@pankod/refine-mui";
 
-const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
+import { ProfileProps, PropertyProps } from "interfaces/common";
+import PropertyCard from "./PropertyCard";
+
+function checkImage(url: any) {
+    const img = new Image();
+    img.src = url;
+    return img.width !== 0 && img.height !== 0;
 }
+
+const Profile = ({ type, name, avatar, email, properties }: ProfileProps) => (
+    <div>Profile</div>
+  
+)
 
 export default Profile
