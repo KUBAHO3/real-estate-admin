66e474bcafcd886fba8ca008988877986ad25bed
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-03-04 17:15:47 +0530

    add agent-profile page

--- src/pages/agent-profile.tsx
+++ src/pages/agent-profile.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useOne } from "@pankod/refine-core";
+import { useParams } from "@pankod/refine-react-router-v6";
 
-const agentProfile = () => {
-  return (
-    <div>agentProfile</div>
-  )
-}
+import { Profile } from "components";
 
-export default agentProfile
+const AgentProfile = () => {
+    const { id } = useParams();
+
+    const { data, isLoading, isError } = useOne({
+        resource: "users",
+        id: id as string,
+    });
+
+    console.log(data);
+
+    const myProfile = data?.data ?? [];
+
+    if (isLoading) return <div>loading...</div>;
+    if (isError) return <div>error...</div>;
+
+    return (
+        <div>Hello Profile</div>
+    );
+};
+
+export default AgentProfile;
