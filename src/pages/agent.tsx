75d68393c3f70abbb2850758eb41674f1c5bd768
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-03-04 16:35:30 +0530

    add agent page

--- src/pages/agent.tsx
+++ src/pages/agent.tsx
@@ -1,44 +1,44 @@
-import { useList } from "@pankod/refine-core"
-import { Box, Typography } from "@pankod/refine-mui"
+import { useList } from "@pankod/refine-core";
+import { Box, Typography } from "@pankod/refine-mui";
 
 import { AgentCard } from "components";
 
 const Agents = () => {
-  const { data, isLoading, isError } = useList({
-    resource: 'users'
-  })
+    const { data, isLoading, isError } = useList({ resource: "users" });
 
-  const allAgents = data?.data ?? [];
+    const allAgents = data?.data ?? [];
 
-  if(isLoading) return <div>Loading...</div>
-  if(isError) return <div>Loading...</div>
-  return (
-    <Box>
-      <Typography fontSize={25} fontWeight={700} color="#11142d">
-        Agent Lists
-      </Typography>
-      <Box
-        mt="20px"
-        sx={{
-          display: 'flex',
-          frexWrap: 'wrap',
-          gap: '20px',
-          backgroundColor: '#fcfcfc',
-        }}
-      >{allAgents.map((agent) =>(
-        <AgentCard 
-          key={agent._id}
-          id={agent._id}
-          name={agent.name}
-          email={agent.email}
-          avatar={agent.avatar}
-          noOfProperties={agent.noOfProperties}
-        />
-      ))}
+    if (isLoading) return <div>loading...</div>;
+    if (isError) return <div>error...</div>;
 
-      </Box>
-    </Box>
-  )
-}
+    return (
+        <Box>
+            <Typography fontSize={25} fontWeight={700} color="#11142d">
+                Agents List
+            </Typography>
 
-export default Agents
+            <Box
+                mt="20px"
+                sx={{
+                    display: "flex",
+                    flexWrap: "wrap",
+                    gap: "20px",
+                    backgroundColor: "#fcfcfc",
+                }}
+            >
+                {allAgents.map((agent) => (
+                    <AgentCard
+                        key={agent._id}
+                        id={agent._id}
+                        name={agent.name}
+                        email={agent.email}
+                        avatar={agent.avatar}
+                        noOfProperties={agent.allProperties.length}
+                    />
+                ))}
+            </Box>
+        </Box>
+    );
+};
+
+export default Agents;
