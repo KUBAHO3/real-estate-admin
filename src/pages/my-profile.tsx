728aa93bf2296262e3aa8aca18cf35d2fcc61a4f
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-03-12 14:42:30 +0530

    add the profile page

--- src/pages/my-profile.tsx
+++ src/pages/my-profile.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { useGetIdentity, useOne } from "@pankod/refine-core";
 
-const myProfile = () => {
-  return (
-    <div>myProfile</div>
-  )
-}
+import { Profile } from "components";
 
-export default myProfile
+const MyProfile = () => {
+    const { data: user } = useGetIdentity();
+    const { data, isLoading, isError } = useOne({
+        resource: "users",
+        id: user?.userid,
+    });
+
+    const myProfile = data?.data ?? [];
+
+    if (isLoading) return <div>loading...</div>;
+    if (isError) return <div>error...</div>;
+
+    return (
+        <Profile
+            type="My"
+            name={myProfile.name}
+            email={myProfile.email}
+            avatar={myProfile.avatar}
+            properties={myProfile.allProperties}
+        />
+    );
+};
+
+export default MyProfile;
